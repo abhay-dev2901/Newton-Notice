964e09cc94850ab1255a9df2ca3734728662f32f
Abhay <149485820+coderking0409@users.noreply.github.com>
2024-09-22 11:03:14 +0530

    handled cors

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}))
+app.use(cors())
 
 app.use(express.json());
 
